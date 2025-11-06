1f832d3633dbd1f80a8083e1471e648707f48b5b
Joel Alex <joelalexvaliyaveed@gmail.com>
2025-11-06 22:26:49 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/cross-word/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
